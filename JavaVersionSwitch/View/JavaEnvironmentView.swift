40448efb9d1facc5fba7ac772976a7b0516a9ca6
blabla-yy <yahaha7777@gmail.com>
2022-09-22 00:09:36 +0800

    ui

--- JavaVersionSwitch/View/JavaEnvironmentView.swift
+++ JavaVersionSwitch/View/JavaEnvironmentView.swift
@@ -9,17 +9,40 @@ import SwiftUI
 
 struct JavaEnvironmentView: View {
     let env: JavaEnvironment
+    @State var isExpanded = false
+
     var body: some View {
-        VStack {
-            Text("Version: \(env.specificationVersion)")
-            Spacer()
+        VStack(alignment: .leading) {
+            HStack {
+                Text(env.specificationVersion)
+                    .font(.title2)
+            }
+
+            Form {
+                Text("java.version: \(env.version)")
+                Text("java.runtime.name: \(env.rtName)")
+            }
+            
         }
-//        .frame(minHeight: 100)
+//        .onTapGesture {
+//            isExpanded.toggle()
+//        }
     }
 }
 
 struct JavaEnvironmentView_Previews: PreviewProvider {
     static var previews: some View {
-        JavaEnvironmentView(env: JavaEnvironment.mock)
+        @State var isOn = false
+        @State var selected = 1
+        return
+            List {
+                //            Picker(selection: $selected, label: Text("JDK")) {
+                JavaEnvironmentView(env: JavaEnvironment.mock).tag(1)
+                JavaEnvironmentView(env: JavaEnvironment.mock).tag(2)
+                JavaEnvironmentView(env: JavaEnvironment.mock).tag(3)
+                JavaEnvironmentView(env: JavaEnvironment.mock).tag(4)
+                //            }
+                //            .pickerStyle(.radioGroup)
+            }
     }
 }
